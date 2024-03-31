9a711ea26f09fe7495fd2438299af595a6315e63
Ranjit-Singh-786 <jiradhey402@gmail.com>
2024-03-31 13:14:51 +0530

    btn attached with route links

--- home/views.py
+++ home/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is homepage")  
+    # return HttpResponse("This is homepage")  
     #HttpResponse for render string, but you will render templates
+    return render(request,'home.html')
 
 def about(request):
     return HttpResponse("This is about page") 
