ab61b313a48645973c76823eba1161d492bd2be3
Ranjit-Singh-786 <jiradhey402@gmail.com>
2024-03-31 10:15:04 +0530

    rout defines various tab

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('',views.index,name='home')
+    path('',views.index,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('service',views.service,name='service'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is homepage")
+    return HttpResponse("This is homepage")  
+    #HttpResponse for render string, but you will render templates
+
+def about(request):
+    return HttpResponse("This is about page") 
+
+
+def contact(request):
+    return HttpResponse("This is contact page") 
+
+def service(request):
+    return HttpResponse("This is service page") 
